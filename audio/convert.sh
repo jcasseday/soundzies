4e0775d8558e38ce4de06b0872d3570d18be1d85
Jason Casseday <jcasseday@gmail.com>
2017-02-19 10:30:51 -0800

    updated audio file location in convert script

--- audio/convert.sh
+++ audio/convert.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-~/Development/ffmpeg-3.2.4/bin/ffmpeg -i ./Dog2.m4a -ac 2 -codec:a libmp3lame -b:a 48k -ar 16000 ../dog_alexa.mp3
+~/Development/ffmpeg-3.2.4/bin/ffmpeg -i needsconverting/Dog2.m4a -ac 2 -codec:a libmp3lame -b:a 48k -ar 16000 ./dog_alexa.mp3
